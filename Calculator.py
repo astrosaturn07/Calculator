588e88750efde82fa199075934447f6aea6f2392
Piyu <bt5766408@gmail.com>
2026-04-27 11:50:57 +0530

    Implement input validation and error messages
    Added input validation for choices and division by zero.

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,6 @@
 print("------------------------------Welcome to my calculator------------------------------")
-
+import math
+import sys
 n1 = int(input("Enter a number to be calculated: "))
 n2 = int(input("Enter a number to be calculated: "))
 
@@ -9,25 +10,32 @@ while True:
     print("Enter 3 to calculate multiplication ")
     print("Enter 4 to calculate power ")
     print("Enter 5 to calculate substraction ")
-    print("Enter 6 to calculate floor division of a number ")
-    print("Enter 7 to calculate modulus of a number ")
+    print("Enter 6 to calculate modulus of a number ")
+    print("Enter 7 to calculate floor division of a number ")
     print("Enter 8 to calculate percentage")
     print("Enter 9 to calculate log, sin, cos")
     choice=0
     choice= int(input("Enter your choice "))
 
+    if choice==0 or choice>9:
+         print("Invalid choice, Try again")
+         continue
+
     if(choice==1):
         sum= n1 + n2
         print("The sum is" , sum)
+
     elif(choice==2):
-       if n2==0:
-           print("It cannot be divided")
-       else:
-           division = n1/n2 
-       print("The division is" , division)
+        if n2==0:
+            print("Cannot divide by 0")
+        else:
+            division = n1/n2 
+            print("The division is" , division)
+    
     elif(choice==3):
        multiplication= n1*n2
        print("The multiplication is" , multiplication)
+
     elif(choice==4):
         a=int(input("Enter the value of power "))
         num= int(input("Choose 1st or 2nd number or 3 for both "))
@@ -42,27 +50,40 @@ while True:
             power2= n2**a
             print("The power of n1 is"  , power1 )
             print("The power of n2 is"  , power2 )
+
     elif(choice==5):
         substraction= n1-n2
         print("The substraction is", substraction )
+
     elif(choice==6):
-        modulus= n1%n2
-        print("The modulus is ", modulus)
+        if n2==0:
+            print("Cannot divide by 0")
+        else:
+            modulus= n1%n2
+            print("The modulus is ", modulus)
+
     elif(choice==7):
-        floor= n1//n2
-        print("The floor division of a number is ", floor)
+        if n2==0:
+            print("Cannot divide by 0")
+        else:
+            floor= n1//n2
+            print("The floor division of a number is ", floor)
+
     elif(choice==8):
-        percentage= (n1/n2)*100
-        print("The percentage is ", percentage)
+        if n2==0:
+            print("Cannot divide by 0")
+        else:
+            percentage= (n1/n2)*100
+            print("The percentage is ", percentage)
+
     elif(choice==9):
-        import math
         Num=int(input("enter 1 for log , 2 for cos , 3 for sin "))
         if(Num==1):
           print("The log of n1 is", math.log(n1),"The log of n2 is", math.log(n2) )
         elif(Num==2):
             print( "The cos of n1 is", math.cos(n1),"The cos of n2 is", math.cos(n2))
         elif(Num==3):
-            print("The sin of n1 is", math.sin(n1),"The cos of n2 is", math.sin(n2)) 
+            print("The sin of n1 is", math.sin(n1),"The sin of n2 is", math.sin(n2)) 
 
     again= input("Continue? yes/no: ")
     if again== "no":
